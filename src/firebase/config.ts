57e354c735356ad941fc62bac57c063ebf6947f3
حُسني <hill.cafeee@gmail.com>
2026-08-29 02:06:56 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "mock-api-key",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mock-project.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mock-project",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "mock-project.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "000000000000",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:000000000000:web:000000000000",
+  apiKey: "AIzaSyC9GYAJT1j1JLqP2OXAK0czHJC5NdUqUHk",
+  authDomain: "studio-9674030533-5f5ae.firebaseapp.com",
+  projectId: "studio-9674030533-5f5ae",
+  storageBucket: "studio-9674030533-5f5ae.firebasestorage.app",
+  messagingSenderId: "1041938611868",
+  appId: "1:1041938611868:web:b5cf7cf22b7a0a10937759"
 };
